422f77e5676fcd7405ddbb546b0f15f2828ca7ce
Norcal23 <joshuakneilson@gmail.com>
2017-12-09 21:45:29 -0500

    update for bootstrap

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -2,10 +2,10 @@ var $imgs = $(".Box");
 
 $(document).ready(function(){
     $(".Box img").on("click", function(){
-        console.log($imgs);
-        console.log(this);
-        $("body").append('<div id="lightbox"></div>');
+        $("body").append('<div class="lightbox"></div>');
         var imgSrc = $(this).attr("src");
+        console.log(imgSrc);
+        $(".lightbox").css({"background-color" : "black", "width" : "100%", "height" : "100%"});
        
     });
     
